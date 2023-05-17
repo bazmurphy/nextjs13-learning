0eba6db054dd1103d1ec9f6a2b689cd6b0349cd0
bazmurphy <bazmurphy@gmail.com>
2023-05-17 13:15:41 +0100

    adjustments

--- app/loading.jsx
+++ app/loading.jsx
@@ -4,12 +4,18 @@
 // the file name MUST be loading.jsx
 // but the component function name CAN BE ANYTHING
 
+// we don't have to bring in useState and isLoading(true)
+// fetch and then set it back to isLoading(false)
+
+// if its a Server Component and you are fetching data
+// and you have that loading.jsx with a loading component it WILL DISPLAY
+
 import React from "react";
 
 const LoadingPage = () => {
   return (
     <div className="loading-container">
-      <p>Loading...</p>
+      <p className="loading-message">Loading...</p>
     </div>
   );
 };
